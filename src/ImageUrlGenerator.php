6135ecbe4577c539409452d8f38eea61ce13b35d
Eser <eser@humantocomputer.com>
2020-03-30 19:00:20 +0200

    Possibilité d'avoir le cache en public

--- src/ImageUrlGenerator.php
+++ src/ImageUrlGenerator.php
@@ -2,18 +2,22 @@
 
 namespace LesBricodeurs\LaravelImage;
 
-use Storage;
-use InvalidArgumentException;
 use Illuminate\Contracts\Foundation\Application;
+use InvalidArgumentException;
+use League\Glide\Responses\LaravelResponseFactory;
+use League\Glide\ServerFactory;
+use Storage;
 
 class ImageUrlGenerator
 {
+
     /**
      * The instance of the Application
      *
      * @var Application
      */
     private $app;
+    private $server;
 
     /**
      * ImageUrlGenerator constructor.
@@ -22,13 +26,20 @@ class ImageUrlGenerator
      */
     public function __construct(Application $app)
     {
+        $this->server = ServerFactory::create([
+            'response'          => new LaravelResponseFactory(),
+            'source'            => Storage::disk(config('images.source'))->getDriver(),
+            'cache'             => Storage::disk(config('images.cache'))->getDriver(),
+            'cache_path_prefix' => config('images.cache_path_prefix'),
+            'base_url'          => config('images.base_url'),
+        ]);
         $this->app = $app;
     }
 
     /**
      * Generate the url to an image
      *
-     * @param $path
+     * @param                           $path
      * @param string|array|\ArrayAccess $modifiers
      * @return string
      */
@@ -42,9 +53,27 @@ class ImageUrlGenerator
             $modifiers = config('images.shortcuts.' . $modifiers) ?: [];
         }
 
-        if (!is_array($modifiers)) {
+        if ( ! is_array($modifiers)) {
             throw new InvalidArgumentException('Modifiers are either a string (see config.shortcut) or an array');
         }
+
+        if (empty($modifiers)) {
+            return Storage::disk(config('images.source'))->url($path);
+        }
+
+
+        try {
+            if ($this->server->cacheFileExists($path, $modifiers) === true) {
+                $path_cache = $this->server->getCachePath($path, $modifiers);
+                $disk = Storage::disk(config('images.cache'));
+
+                if (filter_var($disk->url($path_cache), FILTER_VALIDATE_URL)) {
+                    return $disk->url($path_cache);
+                }
+            }
+        } catch (\Exception $e) {
+        }
+
 
         $config = '';
 
